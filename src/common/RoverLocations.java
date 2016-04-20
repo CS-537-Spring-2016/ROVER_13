f6ad930b67c90b9378675c38c291a70d7114f664
mitalikaushal <mitali.kaushal9@gmail.com>
2016-04-20 12:39:49 +0530

    created rover_13 and updated the locations

--- src/common/RoverLocations.java
+++ src/common/RoverLocations.java
@@ -36,6 +36,7 @@ public class RoverLocations {
 		// test rovers
 		roverHash.put(RoverName.ROVER_00, new Coord(4,20));
 		roverHash.put(RoverName.ROVER_99, new Coord(4,7));
+		roverHash.put(RoverName.ROVER_13, new Coord(13,1));
 	}
 	
 	public synchronized boolean moveRover(RoverName rname, Coord loc){
