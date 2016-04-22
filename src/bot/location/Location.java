fb305986f01cfa5b4e4e133aa8d69d054f1231b4
simoli2293 <spatel2293@gmail.com>
2016-04-22 16:27:26 -0700

    Add location file

---
+++ src/bot/location/Location.java
@@ -0,0 +1,23 @@
+package bot;
+
+public class Location_Command {
+
+private int x;
+private int y;
+Location_Command(int x,int y)
+{
+    this.x=x;
+    this.y=y;
+
+}
+
+public int getx()
+{
+   return x;
+}
+public int gety()
+{
+      return y;
+}
+
+}
