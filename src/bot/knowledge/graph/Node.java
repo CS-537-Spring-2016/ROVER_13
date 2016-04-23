6ad4d79692044926e6aafafc31e9aca878ae39bd
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-23 16:28:12 -0700

    add Node class for graph

---
+++ src/bot/knowledge/graph/Node.java
@@ -0,0 +1,19 @@
+package bot.knowledge.graph;
+
+import enums.Science;
+import enums.Terrain;
+
+/**
+ * Created by tj on 4/23/16.
+ */
+public class Node {
+  private int x;
+  private int y;
+  private Terrain terrain;
+  private Science science;
+  private boolean isOccupied;
+
+  public Node(){
+    
+  }
+}
