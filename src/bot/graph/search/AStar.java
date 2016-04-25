721296e6c9b595d41e1fb67da111f985a7e9aefe
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-24 21:11:48 -0700

    add astar methods

--- src/bot/graph/search/AStar.java
+++ src/bot/graph/search/AStar.java
@@ -1,7 +1,33 @@
 package bot.graph.search;
 
+import bot.graph.Graph;
+import bot.graph.Node;
+
+import java.util.List;
+
 /**
  * Created by tj on 4/24/16.
  */
 public class AStar {
+  private final boolean DEBUG = false;
+  public Graph graph;
+
+  public AStar(){
+
+  }
+
+  private List<Node> execute(Graph graph, Node source, Node dest){
+    return null;
+  }
+
+  public List<Node> search(Graph graph, Node source, Node dest) {
+    List<Node> path = execute(new Graph(graph), new Node(source), new Node(dest));
+    return null;
+  }
+
+  public int minDistance(Graph graph, Node source, Node dest){
+    List<Node> path = execute(new Graph(graph), new Node(source), new Node(dest));
+    int dist = path.size();
+    return -1;
+  }
 }
