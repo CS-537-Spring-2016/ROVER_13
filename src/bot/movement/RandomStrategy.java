93c5940a4d2f6f0c0bf8c70b46e718e64cd5c0d4
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-24 18:36:55 -0700

    update style

--- src/bot/movement/RandomStrategy.java
+++ src/bot/movement/RandomStrategy.java
@@ -5,8 +5,7 @@ import java.util.Random;
 /**
  * Created by tj on 4/19/16.
  */
-public class RandomStrategy implements Strategy 
-{
+public class RandomStrategy implements Strategy {
 
   private Random rng;
 
