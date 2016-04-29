187b57f7bb5c5cd0cb19421bedb75cea2bc92fd0
mitalikaushal <mitali.kaushal9@gmail.com>
2016-04-30 04:11:29 +0530

    Attributes and default constructor created

--- src/common/Cell.java
+++ src/common/Cell.java
@@ -1,5 +1,23 @@
 package common;
 
+import enums.Science;
+import enums.Terrain;
+
 public class Cell {
+	
+	public int xPosition;
+	public int yPosition;
+	private Terrain terrain;
+	private Science science;
+	
+	//For occupied status we will use hasRover boolean attribute
+	private boolean hasRover;
+	
+	//default constructor
+	Cell(){
+		terrain=Terrain.SOIL;
+		science = Science.NONE;
+		hasRover=false;
+	}
 
 }
