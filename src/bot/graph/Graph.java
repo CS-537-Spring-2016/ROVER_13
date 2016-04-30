3adef0a920a2cfa968c8e1c5c3afb28b03b1cf3c
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-30 15:08:37 -0700

    update graph to set destination for all nodes

--- src/bot/graph/Graph.java
+++ src/bot/graph/Graph.java
@@ -86,21 +86,12 @@ public class Graph {
     return adjList.containsKey(key);
   }
 
-  public void prepareSearcableNodes(){
+  public void prepareSearcableNodes(Node dest){
     for(Map.Entry<Node, List<Node>> entry : adjList.entrySet()){
       for(Node node : entry.getValue()){
-        node.prepareForSearch();
+        node.prepareForSearch(dest);
       }
-      entry.getKey().prepareForSearch();
-    }
-  }
-
-  public void setAllDestination(Node dest){
-    for(Map.Entry<Node, List<Node>> entry : adjList.entrySet()){
-      Node key = entry.getKey().setAllDestination(dest);
-      for(Node node : entry.getValue()){
-        node.setDestination(dest);
-      }
+      entry.getKey().prepareForSearch(dest);
     }
   }
 }
