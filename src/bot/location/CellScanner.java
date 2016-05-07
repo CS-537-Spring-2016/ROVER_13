f15741da911434da33ca7d3fa29085d518b0aaef
Tony Guardado <guardado.anthonyj@gmail.com>
2016-05-07 15:15:35 -0700

    add timer and skeleton for CellScanner

---
+++ src/bot/location/CellScanner.java
@@ -0,0 +1,7 @@
+package bot.location;
+
+/**
+ * Created by tj on 5/7/16.
+ */
+public class CellScanner {
+}
---
+++ src/bot/schedule/Timer.java
@@ -0,0 +1,7 @@
+package bot.schedule;
+
+/**
+ * Created by tj on 5/7/16.
+ */
+public class Timer {
+}
