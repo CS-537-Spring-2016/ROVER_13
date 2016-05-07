b8e34b185ec5fabbca9209c5d66e85d03f3cc3b2
Tony Guardado <guardado.anthonyj@gmail.com>
2016-05-07 15:18:19 -0700

    update skeletons

--- src/bot/location/CellScanner.java
+++ src/bot/location/CellScanner.java
@@ -1,7 +1,18 @@
 package bot.location;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tj on 5/7/16.
  */
 public class CellScanner {
+
+  public List<Cell> convertToCells(String jsonScanMap){
+    List<Cell> cells = new ArrayList<>();
+    /*
+    TODO: implement logic to create valid cells here
+     */
+    return cells;
+  }
 }
--- src/bot/schedule/Timer.java
+++ src/bot/schedule/Timer.java
@@ -2,6 +2,39 @@ package bot.schedule;
 
 /**
  * Created by tj on 5/7/16.
+ * base provided by mitali
  */
 public class Timer {
+
+  //start time and endtime attributes
+  long startTime;
+  long stopTime;
+  long difference;
+
+  //start function to record the current time in milliseconds
+  public void start(){
+    startTime = System.currentTimeMillis();
+  }
+  //returns the difference in milliseconds that have elapsed since last start call
+  public long stop(){
+    stopTime = System.currentTimeMillis();
+    difference = stopTime - startTime;
+    System.out.println("ElapsedTime: " +difference);
+    return difference;
+  }
+
+  //getters only
+  public long getStartTime() {
+    return startTime;
+  }
+  public long getStopTime() {
+    return stopTime;
+  }
+  public long getDifference() {
+    return difference;
+  }
+
+
+
+
 }
