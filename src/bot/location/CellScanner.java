443626033842103d0ba1a6b68f22b453bc07f6bd
Tony Guardado <guardado.anthonyj@gmail.com>
2016-05-13 11:19:54 -0700

    remove unused comment

--- src/bot/location/CellScanner.java
+++ src/bot/location/CellScanner.java
@@ -28,19 +28,6 @@ public class CellScanner {
     int cellY;
     MapTile currentTile;
     Cell currentCell;
-//    for(int row = 0; row < tiles.length; row++){
-//      for(int col = 0; col < tiles.length; col++){
-//        cellX = col - offSet + x;
-//        cellY = row - offSet + y;
-//        currentTile = tiles[row][col];
-//        if(cellX >= 0 && cellY >= 0){
-//          currentCell = new Cell(cellX, cellY, currentTile.getTerrain(),
-//                  currentTile.getScience(), currentTile.getHasRover()
-//          );
-//          cells.add(currentCell);
-//        }
-//      }
-//    }
     for(int col = 0; col < tiles.length; col++){
       for(int row = 0; row < tiles.length; row++){
         cellX = col - offSet + x;
