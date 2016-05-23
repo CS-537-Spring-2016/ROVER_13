4fbf989bad5c23ced63ca66143e9f16ccb90c3b9
mitalikaushal <mitali.kaushal9@gmail.com>
2016-05-24 01:50:07 +0530

    Create Run Method for connecting to the server

--- src/swarmBots/ROVER_13.java
+++ src/swarmBots/ROVER_13.java
@@ -6,7 +6,10 @@ import common.MapTile;
 import common.ScanMap;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,7 +48,27 @@ public class ROVER_13 {
 		destinations = new ArrayList<>();
 	}
 	
-	
+	/*
+	 *Connects to the server
+	*/
+	private void run() throws IOException, InterruptedException{
+		//Make connection
+		Socket socket = new Socket(SERVER_ADDRESS, PORT_ADDRESS);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream());
+		
+		//process all messages from server, wait untill server requests roverID
+		
+		while(true){
+			String line = in.readLine();
+			if(line.startsWith("SUBMITNAME")){
+				out.println(rovername);
+				break;
+			}
+		}
+		
+	/*----------------------Rover Logic for Rover 13-----------------------------*/
+	}
   public static void main(String[] args){
    
   }
