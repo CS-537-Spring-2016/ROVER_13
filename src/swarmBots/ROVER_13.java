45d885e920f18f4c1e6a567b4de2cc23552dc051
mitalikaushal <mitali.kaushal9@gmail.com>
2016-05-24 02:45:29 +0530

    Create extractLocation Method

--- src/swarmBots/ROVER_13.java
+++ src/swarmBots/ROVER_13.java
@@ -6,6 +6,7 @@ import common.MapTile;
 import common.ScanMap;
 
 import java.io.BufferedReader;
+import java.io.Externalizable;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -68,7 +69,56 @@ public class ROVER_13 {
 		}
 		
 	/*----------------------Rover Logic for Rover 13-----------------------------*/
+	String line="";
+	boolean stuck = false;
+	boolean blocked = false;
+	
+	String[] cardinals = new String[4];
+	cardinals[0] = "N";//for north
+	cardinals[0] = "E";
+	cardinals[0] = "S";
+	cardinals[0] = "W";
+	
+	String currentDirection = cardinals[0];
+	
+	Coord currentLoc = null;
+	Coord previousLoc = null;
+	
+	Coord roverGroupStartPosition = null;
+	Coord targetLocation = null;
+	
+	/*-------------------------------------Target Location from Swarm server-------------------*/
+	out.println("TARGET_LOC");
+	line = in.readLine();
+	if(line == null){
+		System.out.println(rovername + " check connection to the server");
+		line = "";
 	}
+	if(line.startsWith("TARGET_LOC")){
+		targetLocation = extractLocation(line);
+	}
+	}
+	/*
+	 ********************************************** Supporting Methods*************************************************
+	*/
+	//takes String value from server response and parse out x and y values
+  private Coord extractLocation(String line) {
+		// TODO Auto-generated method stub
+	  int indexOf;
+	  indexOf = line.indexOf(" ");line = line.substring(indexOf + 1);
+	  if(line.lastIndexOf(" ") != -1){
+		  String xString = line.substring(0, line.lastIndexOf(" "));
+		  
+		  String yString = line.substring(line.lastIndexOf(" ") + 1);
+		  return new Coord(Integer.parseInt(xString), Integer.parseInt(yString));
+	  }
+			  
+		return null;
+	}
+
+/*
+ * Runs the client
+*/
   public static void main(String[] args){
    
   }
